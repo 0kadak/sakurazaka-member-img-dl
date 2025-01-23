1ff2d15eb2ae00a17d136f62ac2c92307605394d
0kadak <96250124+0kadak@users.noreply.github.com>
2025-01-23 13:51:33 +0900

    Up and functional; implemented: get_max_page(), get_blog_list(), get_img(). TODO: prompt to select which member(s) images to download

--- scrape_blog.py
+++ scrape_blog.py
@@ -4,12 +4,12 @@ import json
 
 os.system('mkdir wkdir')
 
-# get member-id, member-blog-url, member-name
+# get member-id, member-blog-url, member-name and save it to member_list.json
 def get_member_list():
     if not os.path.exists('wkdir/bloglist.html'):
         os.system('curl https://sakurazaka46.com/s/s46/diary/blog/list -o wkdir/bloglist.html')
 
-    pattern = re.compile(r'<li class="box member-([0-9]{2})">.+?<a href="(.+?)">.+?<p class="name">(.+?)</p>.+?</li>',flags=re.DOTALL)
+    pattern = re.compile(r'<li\s+class="box member-([0-9]{2})">.+?<a\s+href="(.+?)">.+?<p\s+class="name">(.+?)</p>.+?</li>',flags=re.DOTALL)
 
     with open('wkdir/bloglist.html',encoding='utf-8') as f:
         html_str = f.read()
@@ -19,91 +19,94 @@ def get_member_list():
     for entry in result:
         result_nl.append({'id':entry[0],'url':'https://sakurazaka46.com' + entry[1],'name':entry[2].lstrip().rstrip()})
 
-    # print(result_nl)
     json.dump(result_nl,open('wkdir/member_list.json','w',encoding='utf-8'),ensure_ascii=False,indent=2)
 
-# get blog-urls for each member
-def get_blog_list():
-    member_list=[{"id":"03",
-    "url":"https://sakurazaka46.com/s/s46/diary/blog/list?ima=0000&ct=03",
-    "name":"上村 莉菜"
-    }]
-
-    pattern = re.compile(r'\
-                         <ul class="com-blog-part.+?">\
-                         .+?\
-                         <li class="box">\
-                         .+?\
-                         <a href="(.+?)">\
-                         .+?\
-                         <p class="date wf-a">\
-                         (.+?)\
-                        #  ([0-9]{4}/[0-9]{1,2}/[0-9]{1,2})\
-                         </p>\
-                         .+?\
-                         <h3 class="title">\
-                         (.+?)\
-                         </h3>\
-                         .+?\
-                         </ul>\
-                         ',flags=re.DOTALL)
-
+# get maxpage for each member and save it to member_list.json
+def get_maxpage():
+    with open('wkdir/member_list.json',encoding='utf-8') as f:
+        member_list = json.load(f)
+    
     for member in member_list:
         if not os.path.exists(f'wkdir/{member["id"]}.html'):
-            os.system(f'curl "{member["url"]}" -o "wkdir/{member["id"]}.html"')
+            os.system(f'curl "{member["url"]}" -o "wkdir/{member["id"]}.html')
 
         with open(f'wkdir/{member["id"]}.html',encoding='utf-8') as f:
             html_str = f.read()
+        pattern = re.compile(r'<li><a href="/s/s46/diary/blog/list\?ima=0000&page=([0-9]+)&ct=[0-9]{2}&cd=blog">→</a></li>\s*</ul>',flags=re.DOTALL)
+        result = pattern.findall(html_str)
+        assert len(result) > 0, f'something wrong with regex pattern, too many matches: {result}'
+        member["maxpage"]=int(result[0])
+    json.dump(member_list,open('wkdir/member_list.json','w',encoding='utf-8'),ensure_ascii=False,indent=2)
 
-        
-        result = pattern.findall('''<ul class="com-blog-part box3 fxpc"><li class="box"><a href="/s/s46/diary/detail/54514?ima=0000&cd=blog">
-  <div class="wrap-bg">
-    <p class="com-coverbox"><span class="img" style="background-image: url(/images/14/e01/5bb9f015672f20873bb60eface46e/1000_1000_102400.jpg);"></span></p>
-    <div class="txt">
-      <div class="prof fx">
-        <div class="prof-in fx">
-          <p class="ph">
-
-<img src="/images/14/54a/291b5858cbffd4d131d5938a94fd7/400_320_102400.jpg" alt="">
-
-          </p>
-          <p class="name">上村 莉菜</p>
-        </div>
-      </div>
-      <div class="date-title">
-        <p class="date wf-a">2024/1/31</p>
-        <h3 class="title">1月31日</h3>
-      </div>
-      <p class="lead">﻿
-
-
-
-
-遅くなってしまいましたが、
-2024年もよろしくお願いします！
-
-
-
-
-
-今日から2日間、
-ゆいぽんの卒業コンサートですね
-
-初期の頃からずっと...</p>
-    </div>
-    <div class="btn-wrap">
-      <p class="btn-type1s wf-a"><span>MORE</span></p>
-    </div>
-  </div>
-</a></li></ul>''')
-        # result = pattern.findall(html_str)
+# get blog-urls for each member
+def get_blog_list():
+    with open('wkdir/member_list.json',encoding='utf-8') as f:
+        member_list = json.load(f)
+    
+    for member in member_list:
+        with open(f'wkdir/{member["id"]}_blog_list.json','w',encoding='utf-8') as f: pass
+        pattern = re.compile(rf'<li\s+class="box">.*?<a\s+href="(.+?)">.*?<p\s+class="name">{member["name"]}</p>.*?<p\s+class="date\s+wf-a">(.+?)</p>.*?<h3\s+class="title">(.*?)</h3>.*?</a>.*?</li>',flags=re.DOTALL)
         result_nl = []
-        for entry in result:
-            result_nl.append({'url':entry[0],'date':entry[1],'title':entry[2]})
+        for i in range(member["maxpage"]+1):
+            maxbar=10
+            progress = int(maxbar*(i+1)/(member["maxpage"]+1))
+            print(f'processing {member["name"]}\tpage{i}...\t[{"#"*progress}{" "*(maxbar-progress)}]',end='\r')
+            if os.path.getsize(f'wkdir/{member["id"]}_blog_list.json') > 0:
+              with open(f'wkdir/{member["id"]}_blog_list.json',encoding='utf-8') as f:
+                  result_nl=json.load(f)
+            if not os.path.exists(f'wkdir/{member["id"]}_{i}.html'):
+                os.system(f'curl "{member["url"]}&page={i}" -o "wkdir/{member["id"]}_{i}.html"')
 
-        json.dump(result_nl,open(f'wkdir/{member["id"]}_blog_list.json','w',encoding='utf-8'),ensure_ascii=False,indent=2)
+            with open(f'wkdir/{member["id"]}_{i}.html',encoding='utf-8') as f:
+                html_str = f.read()
+            result = pattern.findall(html_str)
+            for entry in result:
+                result_nl.append({'url':entry[0],'date':entry[1],'title':entry[2]})
+        print()
+        json.dump(result_nl,open(f'wkdir/{member["id"]}_blog_list.json','a',encoding='utf-8'),ensure_ascii=False,indent=2)
+
+def get_stat():
+    files=[f for f in os.listdir('wkdir') if os.path.isfile(os.path.join('wkdir',f)) and f.endswith('blog_list.json')]
+    print(files)
+    for f in files:
+        with open(f'wkdir/{f}',encoding='utf-8') as j:
+            data=json.load(j)
+        print(f'{f} has {len(data)} entries')
+
+def get_img():
+    with open('wkdir/member_list.json',encoding='utf-8') as f:
+        member_list = json.load(f)
+    member_list = member_list[:1] # to specify which member to download
+    for member in member_list:
+        if os.name=='nt': os.system(f'mkdir img\{member["id"]}')
+        else: os.system(f'mkdir img/{member["id"]}')
+
+        with open(f'wkdir/{member["id"]}_blog_list.json',encoding='utf-8') as f:
+            blog_list = json.load(f)
+        for i, blog in enumerate(blog_list):
+            maxbar=20
+            progress = int(maxbar*(i+1)/(len(blog_list)))
+            print(f'Img DL [{"#"*progress}{" "*(maxbar-progress)}] {member["name"]}-{blog["title"]}\t\t\t',end='\r')
+
+            blog_id = blog["url"].split('/')[-1].split('?')[0]
+            if not os.path.exists(f'wkdir/{member["id"]}_{blog_id}.html'):
+                os.system(f'curl "https://sakurazaka46.com{blog["url"]}" -o "wkdir/{member["id"]}_{blog_id}.html')
+            with open(f'wkdir/{member["id"]}_{blog_id}.html',encoding='utf-8') as f:
+                html_str = f.read()
+            pattern = re.compile(r'<img\s+src="(.+?)".+?>')
+            result = pattern.findall(html_str)
+            for i,img in enumerate(result):
+                filename= img.split('/')[-1].split('.')[0]
+                if filename.startswith('icon'): continue
+                elif filename.startswith('jasrac'): continue
+                elif filename.startswith('sakurazaka46'): continue
+                os.system(f'curl -s "https://sakurazaka46.com{img}" -o "img/{member["id"]}/{blog_id}_{filename}.jpg"')
+    print('done.')
+
 
 if __name__ == '__main__':
-    # patt=re.compile(r'[0-9]{4}/[0-9]{1,2}/[0-9]{1,2}')
-    # print(patt.findall('2024/1/1'))
-    get_blog_list()
+    # get_member_list()
+    # get_maxpage()
+    # get_blog_list()
+    # get_stat()
+    get_img()
